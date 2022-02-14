8e6fcb40187594ed0adfa4ca523bb298a899b4d1
naiyoma <lankas.aurelia@gmail.com>
2022-02-14 23:34:08 +0300

    add render leads function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = ["l","k"]
+let myLeads = []
 const inputEl  = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -7,9 +7,12 @@ let listItems = " "
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    renderLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++) {
-    listItems += myLeads[i]
-    ulEl.innerHTML += "<li>" + listItems + "</li>"
+function renderLeads() {
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
 }
