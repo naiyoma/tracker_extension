f75f4ff6e5db62ab91c90729a896ac5a4ea5b125
naiyoma <lankas.aurelia@gmail.com>
2022-02-15 12:32:00 +0300

    add delete my leads button

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 let myLeads = []
 const inputEl  = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 let listItems = " "
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+
+}
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = '';
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
     renderLeads()
 })
 
+deleteBtn.addEventListener("dbclick", function() {
+    localStorage.clear()
+    myLeads = [];
+    renderLeads()
+})
 function renderLeads() {
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += `
+        <li>
+            <a href = '${myLeads}' target='_blank'>
+                ${myLeads[i]} 
+            </a>
+        </li>`
     }
     ulEl.innerHTML = listItems
 }
