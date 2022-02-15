52ae89fb3cb82b28bec22975a8291652db22a295
naiyoma <lankas.aurelia@gmail.com>
2022-02-15 15:23:43 +0300

    chrome extension

--- index.js
+++ index.js
@@ -3,36 +3,59 @@ const inputEl  = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+const tabBtn = document.getElementById("tab-btn")
 let listItems = " "
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click", function() {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        let activeTab = tabs[0]
+        let activeTabId = activeTab.id
+   
+    })
+
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    console.log(tabs[0].url)
+})
+
+function render(leads) {
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+        <li>
+            <a href = '${leads}' target='_blank'>
+                ${leads[i]} 
+            </a>
+        </li>`
+    }
+    ulEl.innerHTML = listItems
 }
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = '';
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 })
 
-deleteBtn.addEventListener("dbclick", function() {
+deleteBtn.addEventListener("click", function() {
     localStorage.clear()
     myLeads = [];
-    renderLeads()
+    render(myLeads)
+    ulEl.innerHTML = '';
+    console.log("clicked")
 })
-function renderLeads() {
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-        <li>
-            <a href = '${myLeads}' target='_blank'>
-                ${myLeads[i]} 
-            </a>
-        </li>`
-    }
-    ulEl.innerHTML = listItems
-}
+
